f0c68e34b4fe055db16b5cdfff6fbb2caf43623b
kecze <58257904+kecze@users.noreply.github.com>
2022-03-13 14:46:23 +0100

    Add files via upload

--- s1.py
+++ s1.py
@@ -1,4 +1,5 @@
-import pandas as df
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # full_cap - max capacity of the car
 # SOC - actual at the beginning of charging
@@ -22,15 +23,17 @@ def charging(soc, full_cap, power, ch_time):
         y.append(pow_used)
     while 0.8 * full_cap <= act_cap <= 1 * full_cap and time < ch_time:
         soc_v = (act_cap / full_cap) * 100
-        pow_used = power * (-0.01667 * soc_v ** 3 + 4.771 * soc_v ** 2 - 456.9 * soc_v + 14650)/100
-        #pow_used = power * (-0.013333 * soc_v ** 3 + 3.9 * soc_v ** 2 - 381.2 * soc_v + 12460) / 100
+        #pow_used = power * (-0.01667 * soc_v ** 3 + 4.771 * soc_v ** 2 - 456.9 * soc_v + 14650)/100
+        pow_used = power * (0.005333 * soc_v ** 3 - 1.306 * soc_v ** 2 + 100.4 * soc_v - 2306) / 100
         act_cap += pow_used * 1 / 60
         time += 1
         x.append(time)
         y.append(pow_used)
-    df.plot(x,y)
+    plt.plot(x, y)
+    plt.xlabel('time [min]')
+    plt.ylabel('power [kW]')
     print("po",ch_time,"minutach ładowania SOC będzie wynosiło",round(soc_v),"%")
 
 
-charging(0, 40, 7, 480)
+charging(70, 40, 7, 480)
 
